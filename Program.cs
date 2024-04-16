b77c97eb1639183809681aa45f25f5d25bc5aa20
Isaienko Volodymyr <isaienkovova05@gmail.com>
2024-04-16 21:03:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using TeamWork_CalcOperations;
+//using TeamWork_1;
+
+
+string infixExpression = "(5+3*8/4)%2";
+string rpnExpression = CalcOperation.RPNConverter.ConvertToRPN(infixExpression);
+Console.WriteLine("Infix Expression: " + infixExpression);
+Console.WriteLine("RPN Expression: " + rpnExpression);
+
+
+double result = CalcOperation.RPNCalculator.CalculateRPN(rpnExpression);
+Console.WriteLine("Calculate Result: " + result);
